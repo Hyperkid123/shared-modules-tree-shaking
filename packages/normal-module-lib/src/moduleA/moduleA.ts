83faf7c2c466e6e492536d06600d1c053682d362
Martin Maroši <marvusm.mmi@gmail.com>
2022-11-09 17:14:40 +0100

    Add normal module lib.

---
+++ packages/normal-module-lib/src/moduleA/moduleA.ts
@@ -0,0 +1,5 @@
+const moduleAfn = () => {
+  console.log('This is module A function in mini modules lib')
+}
+
+export default moduleAfn;
---
+++ packages/normal-module-lib/src/moduleB/moduleB.ts
@@ -0,0 +1,5 @@
+const moduleBfn = () => {
+  console.log('This is module B function in mini modules lib')
+}
+
+export default moduleBfn;
